b88949da410e3e12629bee65ca99be7930e83c0f
Peeratchai Petpadriew <p.petpadriew@gmail.com>
2017-04-10 21:28:39 +0700

    Move declartion to separate file.

--- src/index.tsx
+++ src/index.tsx
@@ -3,24 +3,6 @@ import * as ReactDOM from "react-dom";
 const { AppContainer } = require("react-hot-loader");
 import App from "./App";
 
-declare global {
-    interface NodeModule {
-        hot: {
-            accept(dependencies: string[], callback: (updatedDependencies: string[]) => void): void;
-            accept(dependency: string, callback: () => void): void;
-            accept(errHandler?: (err: any) => void): void;
-            decline(dependencies: string[]): void;
-            decline(dependency: string): void;
-            decline(): void;
-
-            dispose(callback: (data: any) => void): void;
-            addDisposeHandler(callback: (data: any) => void): void;
-
-            removeDisposeHandler(callback: (data: any) => void): void;
-        }
-    }
-}
-
 ReactDOM.render(<AppContainer><App /></AppContainer>, document.getElementById("root"));
 
 // Handle hot reloading requests from Webpack
